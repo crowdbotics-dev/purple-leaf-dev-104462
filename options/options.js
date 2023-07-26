cf9321a354813c4a0afe96d47c8180b50be1a331
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 13:51:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-leaf-dev-104462",
+  url: "https://purple-leaf-dev-104462.botics.co",
+  api: "https://purple-leaf-dev-104462.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
